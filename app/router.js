777fc0374df94ffb531540f7f7ac9c6cc73c19aa
Eric Parra <parra@csu.fullerton.edu>
2017-05-03 17:51:38 -0700

    Initial routes and views

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('room');
+  this.route('rooms', function() {
+    this.route('new');
+  });
 });
 
 export default Router;
