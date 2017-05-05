93ff72566e1123a980c3bb5397061e2564617652
Eric Parra <parra@csu.fullerton.edu>
2017-05-04 17:44:33 -0700

    Add card model with example in /room

--- app/routes/room.js
+++ app/routes/room.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    let triviaCard = this.store.createRecord('card', {
+      question: 'Which of the following is NOT a feature of ES6',
+      a: 'Arrow Function Notation',
+      b: 'Enhanced Object Literals',
+      c: 'Bean Validation',
+      d: 'Iterables and iterators',
+      answer: 'c'
+    });
+    return [triviaCard];
+  }
 });
